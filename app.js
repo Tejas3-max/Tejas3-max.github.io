0607a885347a29710cde3331a0017db5141687fe
Tejas3-max <71709306+Tejas3-max@users.noreply.github.com>
2022-10-08 10:51:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const apps = {
 
 const platform = () => {
   let userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  if (/windows phone/i.test(userAgent)) return 'Windows Phone';
+  //if (/windows phone/i.test(userAgent)) return 'Windows Phone';
   if (/android/i.test(userAgent)) return 'Android';
   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) return 'iOS';
   return null;
